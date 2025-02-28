1c474f480cd29d1f486d0480c145a4a37a78a920
Arno Appelgryn <acappelgryn@gmail.com>
2025-02-27 22:03:20 -0600

    Templating with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
+  const name = "StarGazers"
+
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" atl="StarGazers Group" />
+          <h1>Meet the <i style={{color:"SteelBlue"}}>{ name }</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        </hgroup>
+        <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+      </article>
+    </div>
   )
 }
 export default App
