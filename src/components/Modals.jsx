4bd68ecb221b7295c6d6ebfc3135e662b7e1aa8c
Arno Appelgryn <acappelgryn@gmail.com>
2025-03-04 15:05:26 -0600

    Add Custom Fonts and Icons

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -13,19 +13,26 @@ export default ({ member, handleClose, handleChange }) => {
             display: "flex",
             gap: '1rem'
           }}>
-            <img style={{ width: '200px' }}
+            <img
+              style={{ width: '200px' }}
               src={`images/${member.slug}.svg`}
               alt={member.name} />
             <hgroup>
               <h1>{member.name}</h1>
               <p>{member.bio}</p>
-              <hgroup>
+              <hgroup style={{
+                display: "flex",
+                gap: "1rem",
+                marginTop: "1rem",
+                justifyContent: "space-between",
+
+              }}>
                 <a className="outline" href="#" role="button"
                   onClick={() => { handleChange(Number(member.id) - 1) }}
-                >previous</a>
+                ><span className="material-symbols-outlined">arrow_back</span></a>
                 <a className="outline" href="#" role="button"
                   onClick={() => { handleChange(Number(member.id) + 1) }}
-                >next</a>
+                ><span className="material-symbols-outlined">arrow_forward</span></a>
               </hgroup>
             </hgroup>
           </div>
